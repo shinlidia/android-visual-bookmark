7063257946088a56d13195d0b0cc121ca1d4592a
zeegee <zeegee@30984998-66e6-11de-9b35-9190732fe600>
2009-07-02 09:14:43 +0000

    initial commit * WebView snap shot from background service is working

---
+++ VisualBookmarks/src/net/woft/visualbookmarks/SnapshotActivity.java
@@ -0,0 +1,49 @@
+package net.woft.visualbookmarks;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.Browser;
+import android.provider.Browser.BookmarkColumns;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class SnapshotActivity extends Activity {
+
+	private static final String LOGTAG = "SnapshotActivity";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		startService(new Intent(this, SnapshotService.class));
+		findViewById(R.id.snapshot_button).setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				ContentResolver cr = getContentResolver();
+				Cursor cursor = cr.query(Browser.BOOKMARKS_URI, new String[]{BookmarkColumns.URL},
+						null, null, null);
+				cursor.moveToFirst();
+				while(!cursor.isAfterLast()) {
+					Log.v(LOGTAG, "url=" + cursor.getString(0));
+					cursor.moveToNext();
+				}
+				cursor.close();
+			}
+		});
+	}
+	
+	@Override
+	protected void onDestroy() {
+		stopService(new Intent(this, SnapshotService.class));
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+}
